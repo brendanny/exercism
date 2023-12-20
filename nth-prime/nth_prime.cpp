75339ac83e5d248797822e73b48522e923eaf986
Brendan Fattig <54815652+brendanny@users.noreply.github.com>
2023-12-20 04:04:19 -0600

    nth-prime final

--- nth-prime/nth_prime.cpp
+++ nth-prime/nth_prime.cpp
@@ -1,5 +1,34 @@
 #include "nth_prime.h"
+#include <cmath>
+#include <stdexcept>
 
 namespace nth_prime {
+int nth(int prime) {
+  if (prime == 0) {
+    throw std::domain_error("There is no zero-th prime");
+  }
+  if (prime == 1) {
+    return 2;
+  }
+  int count = 1;
+  int i = 2;
+  while (count < prime) {
+    if (is_prime(i)) {
+      count++;
+    }
+    ++i;
+  }
+  return --i;
+}
 
-}  // namespace nth_prime
+bool is_prime(int num) {
+  bool prime = true;
+  for (int i = 2; i < std::sqrt(num) + 1; ++i) {
+    if (num % i == 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
+} // namespace nth_prime
