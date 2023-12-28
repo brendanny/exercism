bda6ab365d4a2c7228c157c4629b46592569b886
Brendan Fattig <54815652+brendanny@users.noreply.github.com>
2023-12-27 22:42:53 -0600

    raindrops final

--- raindrops/raindrops.cpp
+++ raindrops/raindrops.cpp
@@ -1,5 +1,22 @@
 #include "raindrops.h"
 
 namespace raindrops {
+std::string convert(int num) {
+  std::string output{""};
 
-}  // namespace raindrops
+  if (num % 3 == 0) {
+    output += "Pling";
+  }
+  if (num % 5 == 0) {
+    output += "Plang";
+  }
+  if (num % 7 == 0) {
+    output += "Plong";
+  }
+  if (output.empty()) {
+    output = std::to_string(num);
+  }
+
+  return output;
+}
+} // namespace raindrops
--- raindrops/raindrops.h
+++ raindrops/raindrops.h
@@ -1,8 +1,10 @@
 #if !defined(RAINDROPS_H)
 #define RAINDROPS_H
 
-namespace raindrops {
+#include <string>
 
-}  // namespace raindrops
+namespace raindrops {
+std::string convert(int);
+} // namespace raindrops
 
 #endif // RAINDROPS_H
