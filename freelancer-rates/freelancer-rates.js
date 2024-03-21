0d122e64dc85367b023c046baa0f36efeba9142d
Brendan Fattig <brendan@fattig.net>
2024-03-20 23:03:24 -0500

    freelancer-rates final

--- freelancer-rates/freelancer-rates.js
+++ freelancer-rates/freelancer-rates.js
@@ -26,7 +26,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +37,7 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return Math.floor(budget / dayRate(ratePerHour));
 }
 
 /**
@@ -49,5 +49,8 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+  return Math.ceil(
+    Math.floor(numDays / 22) * 22 * dayRate(ratePerHour) * (1 - discount) +
+      (numDays % 22) * dayRate(ratePerHour)
+  );
 }
