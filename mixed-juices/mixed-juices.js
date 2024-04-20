a1d59b44d97c698fa650402832800545112e2cda
Brendan Fattig <brendan@fattig.net>
2024-04-20 17:17:37 -0500

    mixed-juices final

--- mixed-juices/mixed-juices.js
+++ mixed-juices/mixed-juices.js
@@ -11,7 +11,19 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5;
+    case "Energizer":
+    case "Green Garden":
+      return 1.5;
+    case "Tropical Island":
+      return 3;
+    case "All or Nothing":
+      return 5;
+    default:
+      return 2.5;
+  }
 }
 
 /**
@@ -23,7 +35,22 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+  let limesNeeded = 0;
+  for (let i = 0; i < limes.length && wedgesNeeded > 0; i++) {
+    switch (limes[i]) {
+      case "small":
+        wedgesNeeded -= 6;
+        break;
+      case "medium":
+        wedgesNeeded -= 8;
+        break;
+      case "large":
+        wedgesNeeded -= 10;
+        break;
+    }
+    limesNeeded++;
+  }
+  return limesNeeded;
 }
 
 /**
@@ -34,5 +61,11 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  if (timeLeft <= 0) {
+    return orders;
+  }
+  return remainingOrders(
+    timeLeft - timeToMixJuice(orders[0]),
+    orders.splice(1)
+  );
 }
