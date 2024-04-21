5e121ad25f47460971f5e33790056592eda2f0b2
Brendan Fattig <brendan@fattig.net>
2024-04-21 02:06:53 -0500

    lasagna-master final

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -10,3 +10,49 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime) {
+  switch (remainingTime) {
+    case undefined:
+      return "You forgot to set the timer.";
+    case 0:
+      return "Lasagna is done.";
+    default:
+      return "Not done, please wait.";
+  }
+}
+
+export function preparationTime(layers, avgTimePerLayer = 2) {
+  return layers.length * avgTimePerLayer;
+}
+
+export function quantities(layers) {
+  let numNoodles = 0;
+  let numSauce = 0;
+  for (let i = 0; i < layers.length; i++) {
+    switch (layers[i]) {
+      case "noodles":
+        numNoodles++;
+        break;
+      case "sauce":
+        numSauce++;
+        break;
+    }
+  }
+  return {
+    noodles: numNoodles * 50,
+    sauce: numSauce * 0.2,
+  };
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  myList.push(friendsList[friendsList.length - 1]);
+}
+
+export function scaleRecipe(recipe, scalar) {
+  let scaledRecipe = { ...recipe };
+  for (let key in scaledRecipe) {
+    scaledRecipe[key] *= scalar / 2;
+  }
+  return scaledRecipe;
+}
