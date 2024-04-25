7d22fa0efa219edd140855527c82b7bbdc0c3eef
Brendan Fattig <brendan@fattig.net>
2024-04-24 21:39:35 -0500

    elyses-transformative-enchantments final

--- elyses-transformative-enchantments/enchantments.js
+++ elyses-transformative-enchantments/enchantments.js
@@ -8,7 +8,7 @@
  * @returns {number[]} deck with every card doubled
  */
 export function seeingDouble(deck) {
-  throw new Error("Implement the seeingDouble function");
+  return deck.map((card) => card * 2);
 }
 
 /**
@@ -19,7 +19,16 @@ export function seeingDouble(deck) {
  * @returns {number[]} deck with triplicate 3s
  */
 export function threeOfEachThree(deck) {
-  throw new Error("Implement the threeOfEachThree function");
+  let newDeck = [];
+  deck.forEach((card) => {
+    newDeck.push(card);
+    if (card === 3) {
+      newDeck.push(card);
+      newDeck.push(card);
+    }
+  });
+
+  return newDeck;
 }
 
 /**
@@ -31,7 +40,10 @@ export function threeOfEachThree(deck) {
  * @returns {number[]} deck with only two middle cards
  */
 export function middleTwo(deck) {
-  throw new Error("Implement the middleTwo function");
+  return deck.slice(
+    Math.floor(deck.length / 2) - 1,
+    Math.floor(deck.length / 2) + 1
+  );
 }
 
 /**
@@ -43,7 +55,8 @@ export function middleTwo(deck) {
  */
 
 export function sandwichTrick(deck) {
-  throw new Error("Implement the sandwichTrick function");
+  deck.splice(Math.floor(deck.length / 2), 0, deck[deck.length - 1], deck[0]);
+  return deck.slice(1, -1);
 }
 
 /**
@@ -54,7 +67,7 @@ export function sandwichTrick(deck) {
  * @returns {number[]} deck with only 2s
  */
 export function twoIsSpecial(deck) {
-  throw new Error("Implement the twoIsSpecial function");
+  return deck.filter((card) => card === 2);
 }
 
 /**
@@ -65,7 +78,8 @@ export function twoIsSpecial(deck) {
  * @returns {number[]} ordered deck
  */
 export function perfectlyOrdered(deck) {
-  throw new Error("Implement the perfectlyOrdered function");
+  deck.sort((a, b) => a - b);
+  return deck;
 }
 
 /**
@@ -76,5 +90,6 @@ export function perfectlyOrdered(deck) {
  * @returns {number[]} reordered deck
  */
 export function reorder(deck) {
-  throw new Error("Implement the reorder function");
+  deck.reverse();
+  return deck;
 }
