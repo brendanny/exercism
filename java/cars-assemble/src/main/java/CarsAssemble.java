66fd587efe7976ba9c111ab47a887179dd0bd10e
Brendan Fattig <brendan@fattig.net>
2024-04-26 19:12:39 -0500

    java: cars-assemble final

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,23 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        int carsPerHour = 221;
+        double productionRate = 0;
+        if (speed <= 4) {
+            productionRate = speed * carsPerHour;
+        } else if (speed <= 8) {
+            productionRate = 0.9 * speed * carsPerHour;
+        } else if (speed == 9) {
+            productionRate = 0.8 * speed * carsPerHour;
+        } else if (speed == 10) {
+            productionRate = 0.77 * speed * carsPerHour;
+        } else {
+            productionRate = 0;
+        }
+        return productionRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) Math.floor(productionRatePerHour(speed) / 60);
     }
 }
