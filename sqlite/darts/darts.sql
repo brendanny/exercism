41060acb105019086ece66f95844841082af3f0c
Brendan Fattig <brendan@fattig.net>
2024-05-03 16:31:17 -0500

    sqlite: darts final

--- sqlite/darts/darts.sql
+++ sqlite/darts/darts.sql
@@ -1,2 +1,9 @@
 -- Schema: CREATE TABLE "darts" ("x" REAL, "y" REAL, score INTEGER);
 -- Task: update the darts table and set the score based on the x and y values.
+update darts
+set score = case
+        when sqrt(power(x, 2) + power(y, 2)) <= 1 then 10
+        when sqrt(power(x, 2) + power(y, 2)) <= 5 then 5
+        when sqrt(power(x, 2) + power(y, 2)) <= 10 then 1
+        else 0
+    end;
